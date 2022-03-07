0f07352058bf1eb7a7a52bcbd324141edf579dca
SupakVetanat <ve.supak_st@tni.ac.th>
2022-03-07 13:50:21 +0700

    Cart

--- src/Pages/CartPage.js
+++ src/Pages/CartPage.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import { InnerLayout, MainLayout } from '../styles/Layout'
 import Title from '../Components/Title'
+import Cart from '../Components/Cart'
 
 const CartPage = () => {
   return (
@@ -8,6 +9,7 @@ const CartPage = () => {
     <MainLayout>
             <Title title={'Cart'} span={'Cart'}/>
               <InnerLayout>
+                <Cart/>
                 </InnerLayout>
              
     </MainLayout>
